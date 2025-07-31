d761d25c96198b522d44c1372f22d2a8da998201
Durgarao Chepuri <durgaraochepuri9@gmail.com>
2025-07-31 15:59:23 +0530

    first commit by durga

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to RxHMS E-com Web</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
